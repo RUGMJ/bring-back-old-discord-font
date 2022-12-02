ff47e79ad4f709d69730d033620b37bd07366495
rugmj <rugmj@rugmj.dev>
2022-12-02 17:13:37 +0000

    Reworked Style Changing

--- bring-back-old-discord-font.user.js
+++ bring-back-old-discord-font.user.js
@@ -1,14 +1,18 @@
 // ==UserScript==
-// @name         Discord custom nicknames
+// @name         Bring Back Old Discord Font
 // @namespace    https://github.com/rugmj/bring-back-old-discord-font
 // @version      0.3.5
 // @description  Reverts the discord font back to the old one
 // @author       RUGMJ
 // @match        https://discord.com/channels/*
 // @icon         https://www.google.com/s2/favicons?domain=discord.com
-// @resource      customCss https://raw.githubusercontent.com/RUGMJ/bring-back-old-discord-font/main/custom.css
 // @grant         GM_addStyle
 // @grant         GM_getResourceText
+// @run-at        document-start
 // ==/UserScript==
 
-GM_addStyle(GM_getResourceText('customCss'));
+GM_addStyle(`
+    :root {
+        --font-primary: "Noto Sans" !important;
+    }
+`);
